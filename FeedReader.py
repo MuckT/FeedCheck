cf88699145c6f251f2d51ba6116a9143987f1881
Tom <Mcgtom10@gmail.com>
2017-05-02 01:20:03 -0700

    Updated saves, searches now return tuples.
    Fixed File saving for both lists and dicts. Added code to retain top lines in file and feed searches. Provided checks to prevent empty files of just top-line information from being saved.

--- FeedReader.py
+++ FeedReader.py
@@ -8,6 +8,15 @@ import calendar
 Feed = {}
 
 #Functions
+#Setting up working directory to_dict on all files in folder
+def start():
+	global Feed
+	apath = raw_input("Insert location of GTFS Folder Unzipped... ")
+	print timestamp() # Start Timer
+	os.chdir(apath)
+	Feed = to_feed(file_walk(apath))
+	return None
+	
 #Finds all .txt Files in a Folder
 def file_walk(s):
 	txtFiles = []
@@ -17,7 +26,7 @@ def file_walk(s):
 				txtFiles.append(f)
 	return txtFiles
 	
-#Makes a Dictionary Entry e.g. {"stops.txt":[["...","...","..."]["...","...","..."]]
+#Makes a Dictionary Entry e.g. "stops.txt":[["...","...","..."]["...","...","..."]]
 def to_dict(s):
 	f_dict = {}
 	f_list = []
@@ -27,14 +36,13 @@ def to_dict(s):
 	f_dict = {s: f_list}
 	#print f_dict[] # Check Top Line Values
 	return f_dict
-
+	
 #Calls to_dict and Updates Global Feed
 def to_feed(s):
 	global Feed
 	local_feed = {}
 	for item in s:
 		tmp_entry = to_dict(item)
-		Feed.update(tmp_entry)
 		local_feed.update(tmp_entry)
 	return local_feed
 	
@@ -50,18 +58,18 @@ def file_search(file, field, *args):
 	output = []
 	hit_list = []
 	try:
-		#output.append([Feed[file][0]]) #Retain Top Line
+		output.append(Feed[file][0]) #Retain Top Line
 		for arg in args:
 			hit_list.append(str(arg))
 		for row in Feed[file]:
 			if row[indexer(file,field)] in hit_list:
-				output.append([row])
+				output.append(row)
 	except:
 		print "File Search Failed"
-	return output
+	return [output, file]
 
 #Removes All Rows with Entries in a Fields that Match Args
-def remove(file, field, *args):
+def remove_rows(file, field, *args):
 	global Feed
 	output = []
 	remove_list = []
@@ -70,12 +78,12 @@ def remove(file, field, *args):
 			remove_list.append(str(arg))
 		for row in Feed[file]:
 			if row[indexer(file,field)] not in remove_list:
-				output.append([row])
+				output.append(row)
 			else:
 				pass
 	except:
 		print "Remove Failed"
-	return output
+	return [output, file]
 			
 def feed_search(*args):
 	global Feed
@@ -87,10 +95,15 @@ def feed_search(*args):
 			hit_list.append(arg)
 		for items in Feed:
 			filematch = []
+			filematch.append(Feed[items][0]) #Populates Top Line
+			count = 0
 			for row in Feed[items]:
 				for item in row:
 					if item in hit_list:
-						filematch.append([row])
+						count += 1
+						filematch.append(row)
+			if count == 0:
+				filematch.pop(0) #Pop's Off Top Line if List is Otherwise Empty
 			match = {items: filematch}
 			hits.update(match)
 	except:
@@ -105,63 +118,72 @@ def timestamp():
 #Writes a list or a dict to File Folder	
 def write_to_file(s):
 	if isinstance(s, list) == True:
-		with open("output.csv", "wb") as f:
-			writer = csv.writer(f)
+		print "Saving File!"
+		try:
+			filename = "%s_%s.txt" % (timestamp(),str(s[1]))
+			with open(filename, 'wb') as f:
+				writer = csv.writer(f)
+				for row in s[0]:
+					writer.writerow(row)
+		except:
+			print "Save List to File Failed"
+			
 	elif isinstance(s, dict) == True:
-		for item in s:
-			if s[item] != []: # Prevents Writing Empty Files
-				filename = "%s_%s" % (timestamp(), item )
-				with open(filename, "wb") as f:
-					writer = csv.writer(f)
-					for item in s[item]:
-						writer.writerows(item)
+		print "Saving Files!"
+		try:
+			for item in s:
+				if s[item] != []: # Prevents Writing Empty Files
+					filename = "%s_%s" % (timestamp(), item)
+					with open(filename, 'wb') as f:
+						writer = csv.writer(f)
+						writer.writerows(s[item][0:])
+		except:
+			print "Save Dict to File Failed!"
+
 	else:
 		pass
-			
-
-#Should be __init__?
-#Setting up Working Directory & Running Functions
-apath = raw_input("Insert location of GTFS Folder Unzipped... ")
-print timestamp() # Start Timer
-os.chdir(apath)
-to_feed(file_walk(apath)) #feed read __init__?
+		
+#Initialize Feed		
+start()
 
 #Script Samples
 #Remove any Number of rows Based on Matching in One Field.
-#print remove("fare_attributes.txt", "price", "1.25")
+#print remove_rows("fare_attributes.txt", "price", "1.25")
 
 #File Search any Number of Items Based on Matching in One Field.
 #print file_search("stops.txt", "stop_id", "NANAA", "DADAN")
 #print file_search("trips.txt", "route_id", "AAMV")
 
 #write to file Writes "output_" + timestamp.csv to Working Directory / Feed Location
-#write_to_file(remove("stop_times.txt", "trip_id", "CITY2", "CITY1))
-#write_to_file(remove("routes.txt" , "route_id" , "BFC")
+#write_to_file(remove_rows("stop_times.txt", "trip_id", "CITY2", "CITY1"))
+#write_to_file(remove_rows("frequencies.txt", "headway_secs" , "1800"))
 
 #write_to_file(file_search("routes.txt", "agency_id", "DTA"))
 #write_to_file(file_search("frequencies.txt", "headway_secs" , "600", "1800"))
 
-#Random Search to Test Speed
-feed_search("-116.751677")
-remove("frequencies.txt", "headway_secs" , "600", "1800")
+
+#Function Calls to Test Speed
+feed_search("AB")
+remove_rows("frequencies.txt", "headway_secs" , "1800")
+remove_rows("frequencies.txt", "headway_secs" , "1800")
+remove_rows("stops.txt", "stop_name", "King St and S West St", "Mt Vernon Ave and E Mason Ave", "King St Metro Station - Bay B")
 file_search("stops.txt" , "stop_id" , "NANAA" , "BULLFROG" , "FUR_CREEK_RES")
 file_search("trips.txt" , "route_id" , "STBA" , "AB" , "BFC")
 
 print timestamp()# End Timer
-
 """
 #Showing How Some of The Data in Feed Looks
 #Print First Five Rows of all Entries in Global Feed
 for item in Feed:
 	print Feed[item][0:5]
+	
 #Print First Row of all Entries in Global Feed
 for item in Feed:
 	print Feed[item][0]
-
+"""
 #This is fun glitch art. Prints out the contents of the file. Ctrl+ C for KeyboardInterrupt.
-print Feed["stops.txt"][0:]
+#print Feed["stops.txt"][0:]
 #print Feed["trips.txt"][0:]
 #print Feed["stops.txt"][0:]
 #print Feed["agency.txt"][0:]
 #print Feed["stop_times.txt"][0:]
-"""
